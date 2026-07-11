f96358078b781f4d47fb9c262716e5365afaed3a
Malaika Shaukat <malaikashaukat871@gmail.com>
2026-07-11 18:44:17 +0500

    Resolve merge conflicts and finalize backend updates

--- src/embedder.py
+++ src/embedder.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import logging
 
 import numpy as np
@@ -44,18 +43,3 @@ class Embedder:
             vec = vec.mean(axis=0)
 
         return vec
-=======
-from src.services import ServiceHub
-
-
-class Embedder:
-
-    def __init__(self):
-        self.model = ServiceHub.get_embedding_model()
-
-    def embed_documents(self, texts):
-        return self.model.encode(texts)
-
-    def embed_query(self, text):
-        return self.model.encode([text])[0]
->>>>>>> 41cac17 (INitial COmmit)
--- src/services.py
+++ src/services.py
@@ -1,6 +1,7 @@
-<<<<<<< HEAD
 import os
+
 from huggingface_hub import InferenceClient
+
 from config import EMBEDDING_MODEL, LLM_MODEL
 
 
@@ -32,34 +33,3 @@ class ServiceHub:
     @classmethod
     def get_embedding_client(cls):
         return cls.get_client()
-=======
-from sentence_transformers import SentenceTransformer
-from huggingface_hub import InferenceClient
-from config import EMBEDDING_MODEL, LLM_MODEL
-import os
-
-
-class ServiceHub:
-    _embedding_model = None
-    _llm_client = None
-
-    @classmethod
-    def get_embedding_model(cls):
-
-        if cls._embedding_model is None:
-            print("Loading embedding model...")
-            cls._embedding_model = SentenceTransformer(EMBEDDING_MODEL)
-
-        return cls._embedding_model
-
-    @classmethod
-    def get_llm_client(cls):
-
-        if cls._llm_client is None:
-            print("Loading LLM client...")
-            cls._llm_client = InferenceClient(
-                token=os.getenv("HF_TOKEN")
-            )
-
-        return cls._llm_client
->>>>>>> 41cac17 (INitial COmmit)
